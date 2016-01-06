8f3e4893a5e2b878b94ff11b584af6b195a3f75b
Daniel Marcos <danielmarcos.to@gmail.com>
2016-01-06 14:04:45 +0100

    Added ball and bottom collider

---
+++ Assets/Scrips/LoseCollider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D trigger) {
+		Debug.Log ("Trigger");
+	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		Debug.Log ("Collision");
+	}
+}
