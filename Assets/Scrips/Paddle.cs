e50ea708ca7c503b3fcd311e6c0c1cd5e615139f
Daniel Marcos <danielmarcos.to@gmail.com>
2016-01-06 20:28:05 +0100

    Made the movement of the padle

---
+++ Assets/Scrips/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mousePositionInBlocks = ((Input.mousePosition.x / Screen.width) * 16) - 8;
+
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, this.transform.position.z);
+
+		paddlePos.x = Mathf.Clamp (mousePositionInBlocks, -7.5f, 7.5f);
+		this.transform.position = paddlePos;
+		Debug.Log (mousePositionInBlocks);
+	}
+}
