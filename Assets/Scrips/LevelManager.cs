baf57ac0da102131a794d08fa492f29eb261e5c7
Daniel Marcos <danielmarcos.to@gmail.com>
2016-01-08 12:06:26 +0100

    Deleted debug logs

--- Assets/Scrips/LevelManager.cs
+++ Assets/Scrips/LevelManager.cs
@@ -5,13 +5,11 @@ using System.Collections;
 public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel (string name) {
-		//Debug.Log ("Level load requested for: " + name);
 		Brick.breakableCount = 0;
 		SceneManager.LoadScene (name);
 	}
 
 	public void LoadNextLevel () {
-		//Debug.Log (SceneManager.GetActiveScene().buildIndex);
 		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
 		Brick.breakableCount = 0;
 	}
@@ -23,7 +21,6 @@ public class LevelManager : MonoBehaviour {
 	}
 
 	public void QuitRequest () {
-		//Debug.Log ("I want to quit!");
 		Application.Quit ();
 	}
 }
