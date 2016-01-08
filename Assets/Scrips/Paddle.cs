6ac1f654324845b44c78087e37c895535a896b3a
Daniel Marcos <danielmarcos.to@gmail.com>
2016-01-08 11:13:51 +0100

    Added autoplay mode for testing

--- Assets/Scrips/Paddle.cs
+++ Assets/Scrips/Paddle.cs
@@ -3,22 +3,37 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour {
 
+	public bool autoPlay = false;
+
+	private Ball ball;
+
 	// Use this for initialization
 	void Start () {
-	
+		ball = GameObject.FindObjectOfType<Ball> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		MovePaddle ();
+		if (autoPlay) {
+			AutoMove ();
+		} else {
+			MoveWithMouse ();
+		}
 	}
 
-	void MovePaddle () {
+	void MoveWithMouse () {
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
 		float mousePositionInBlocks = ((Input.mousePosition.x / Screen.width) * 16) - 8;
 
-		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, this.transform.position.z);
-
 		paddlePos.x = Mathf.Clamp (mousePositionInBlocks, -7.5f, 7.5f);
+		this.transform.position = paddlePos;
+	}
+
+	void AutoMove() {
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
+		Vector3 ballPosition = ball.transform.position;
+
+		paddlePos.x = Mathf.Clamp (ballPosition.x, -7.5f, 7.5f);
 		this.transform.position = paddlePos;
 	}
 }
