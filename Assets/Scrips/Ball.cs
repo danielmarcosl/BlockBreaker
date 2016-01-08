2bb78b480a43986a43f9672df684e6c19ea65eda
Daniel Marcos <danielmarcos.to@gmail.com>
2016-01-08 10:56:02 +0100

    Order code

--- Assets/Scrips/Ball.cs
+++ Assets/Scrips/Ball.cs
@@ -16,24 +16,31 @@ public class Ball : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (!hasStarted) {
-			// Lock the ball relative to the paddle at the start
-			this.transform.position = paddle.transform.position + paddleToBallVector;
+			LaunchBallAtStart ();
+		}
+	}
 
-			// Wait until the left button of the mouse is pressed
-			if (Input.GetMouseButtonDown (0)) {
-				this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (2f, 10f);
+	void LaunchBallAtStart() {
+		// Lock the ball relative to the paddle at the start
+		this.transform.position = paddle.transform.position + paddleToBallVector;
 
-				hasStarted = true;
-			}
+		// Wait until the left button of the mouse is pressed
+		if (Input.GetMouseButtonDown (0)) {
+			this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (2f, 10f);
+
+			hasStarted = true;
 		}
 	}
 
 	void OnCollisionEnter2D (Collision2D col) {
+		ChangeVelocity ();
+		PlayBounceSound (col);
+	}
+
+	void ChangeVelocity () {
 		Vector2 tweak = new Vector2 (Random.Range(0f,0.2f), Random.Range(0,0.2f));
 
 		this.GetComponent<Rigidbody2D> ().velocity += tweak;
-
-		PlayBounceSound (col);
 	}
 
 	void PlayBounceSound (Collision2D col) {
