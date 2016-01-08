c37b154fa9fcaafc158f494b4f590c660f5e444e
Daniel Marcos <danielmarcos.to@gmail.com>
2016-01-08 12:05:03 +0100

    Reset breakableCount at load new Scene for fix an error

--- Assets/Scrips/LevelManager.cs
+++ Assets/Scrips/LevelManager.cs
@@ -6,12 +6,14 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel (string name) {
 		//Debug.Log ("Level load requested for: " + name);
+		Brick.breakableCount = 0;
 		SceneManager.LoadScene (name);
 	}
 
 	public void LoadNextLevel () {
 		//Debug.Log (SceneManager.GetActiveScene().buildIndex);
 		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+		Brick.breakableCount = 0;
 	}
 
 	public void BrickDestroyed () {
