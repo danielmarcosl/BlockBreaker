fc139eb43027f44a516371e231923c5ee1a5d584
Daniel Marcos <danielmarcos.to@gmail.com>
2016-01-08 10:47:54 +0100

    Export the content of Update to a method

--- Assets/Scrips/Paddle.cs
+++ Assets/Scrips/Paddle.cs
@@ -10,12 +10,15 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		MovePaddle ();
+	}
+
+	void MovePaddle () {
 		float mousePositionInBlocks = ((Input.mousePosition.x / Screen.width) * 16) - 8;
 
 		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, this.transform.position.z);
 
 		paddlePos.x = Mathf.Clamp (mousePositionInBlocks, -7.5f, 7.5f);
 		this.transform.position = paddlePos;
-		//Debug.Log (mousePositionInBlocks);
 	}
 }
